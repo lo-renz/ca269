4a9366a36933c6d2b2af1a98b5a72387a91a0438
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-01-19 14:22:00 +0000

    Hello World!

---
+++ week_01/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+*The HelloWorldApp class implements an application that
+*simply prints "Hello World!" to standard output.
+*/
+
+public class HelloWorld {
+
+    /**
+    * @param args the command line arguments
+    */
+
+    public static void main(String[] args) {
+        //Display the string.
+        System.out.println("Hello World!");
+    }
+
+}
