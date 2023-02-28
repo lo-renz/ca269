f2185af8c258f1c6a2b43b4185e748b886db1d98
Renso Guilalas <rensoglorenzo@gmail.com>
2023-02-28 16:08:50 +0000

    Completed ShopInventory2.java

--- week_06/lab/ShopInventory2.java
+++ week_06/lab/ShopInventory2.java
@@ -1,7 +1,8 @@
-import java.util.List;
 import java.util.Stack;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 class Item {
     final String name;
@@ -56,7 +57,11 @@ class Checkout {
     }
 
     public Item removeItem() {
-        return this.checkout.remove();
+        return this.checkout.poll();
+    }
+
+    public boolean empty() {
+        return this.checkout.isEmpty();
     }
 
     public String toString() {
@@ -64,23 +69,60 @@ class Checkout {
     }
 }
 
-public class ShopInventory2 {
-    static void loadBasket(Basket basket) {
-        basket.addItem(new Item("Twinings Earl Grey Tea", 4.50));
-        basket.addItem(new Item("Folans Orange Marmalade", 4.00));
-        basket.addItem(new Item("Free-range Eggs", 3.55));
-        basket.addItem(new Item("Brennan's Bread", 2.15));
-        basket.addItem(new Item("Ferrero Rocher", 7.00));
-        basket.addItem(new Item("Ferrero Rocher", 7.00));
-        //basket.removeItem();
-        //basket.removeItem();
+class Bill {
+    private final Map<String, Integer> basket;
+    private double price;
+    public int count = 1;
+
+    Bill(Checkout checkout) {
+        this.basket = new LinkedHashMap<String, Integer>();
+
+        while(!checkout.empty()) {
+            billItem(checkout.removeItem());
+        }
     }
 
+    public void billItem(Item item) {
+        if (this.basket.containsKey(item.name)) {
+            this.basket.put(item.name, this.basket.get(item.name) + 1);
+            this.price += item.price;
+        }
+        else {
+            this.basket.put(item.name, this.count);
+            this.price += item.price;
+        }
+    }
+
+    public double getTotal() {
+        return this.price;
+    }
+
+    public String toString() {
+        String output = "";
+        for(String item: this.basket.keySet()) {
+            output += item + " (" + this.basket.get(item) + "nos)\n";
+        }
+        return output + "total: " + this.price;
+    }
+}
+
+public class ShopInventory2 {
     public static void main(String[] args) {
         Basket basket = new Basket();
         loadBasket(basket);
-        System.out.println(basket.empty());
+        // System.out.println(basket); // for DEBUG
         Checkout checkout = new Checkout(basket);
-        System.out.println(checkout);
+        //System.out.println(checkout); // for DEBUG
+        Bill bill = new Bill(checkout);
+        System.out.println(bill);
+    }
+
+    static void loadBasket(Basket basket) {
+        basket.addItem(new Item("Twinings Earl Grey Tea", 4.50));
+        basket.addItem(new Item("Folans Orange Marmalade", 4.00));
+        basket.addItem(new Item("Free-range Eggs", 3.35));
+        basket.addItem(new Item("Brennan's Bread", 2.15));
+        basket.addItem(new Item("Ferrero Rocher", 7.00));
+        basket.addItem(new Item("Ferrero Rocher", 7.00));
     }
 }
