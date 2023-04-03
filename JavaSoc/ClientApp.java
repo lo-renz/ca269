12e1e805ab7f39ee3246b76fb602e1e52e0c95d2
Renso Guilalas <rensoglorenzo@gmail.com>
2023-04-03 22:41:11 +0100

    Start of project.

--- JavaSoc/ClientApp.java
+++ JavaSoc/ClientApp.java
@@ -1,11 +1,50 @@
 package JavaSoc;
 
-interface App {
-    Inbox getInbox();
-    Outbox getOutbox();
-    String demo();
+interface Activity {
+    String getURI();
 }
 
-class Inbox {}
+/* receives a message and adds it to the Inbox */
+interface ReceiveMessage {
+    // returns a success / failure message
+    boolean receive(Activity a);
+}
 
-class Outbox {}
+/* removes and retrieves the next message from inbox */
+interface ReadNextMessage {
+    // returns an Activity, or null if there are no messages
+    Activity readNext();
+}
+
+/* provides inbox functionality */
+interface Inbox extends ReceiveMessage, ReadNextMessage {
+    // returns count of unread messages in inbox
+    int getCount();
+}
+
+/* sends a message and adds it to the Outbox */
+interface SendMessage {
+    boolean send(Activity a);
+}
+
+/* removes and delivers the next message from inbox */
+interface DeliverNextMessage {
+    // returns an Activity, or null if there are no messages
+    Activity deliverNext();
+}
+
+/* provides outbox functionality */
+interface Outbox extends SendMessage, DeliverNextMessage {
+    // returns count of unsent messages in outbox
+    int getCount();
+}
+
+/* the client App that handles inboxes and outboxes */
+interface App {
+    Inbox getInbox(); // retrieves the inbox
+    Outbox getOutbox(); // retrievs the outbox
+    String demo(); // prints a demo of the app in action
+}
+
+public class ClientApp implements App {
+}
