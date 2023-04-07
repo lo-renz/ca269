cda8429372b8dbbfa5b26f7bea34fab1dd11af5a
Renso Guilalas <rensoglorenzo@gmail.com>
2023-04-08 00:24:11 +0100

    some work.

---
+++ mock_exam/practice/Set1.java
@@ -0,0 +1,9 @@
+import java.util.Iterator;
+import java.util.iterable;
+
+class Test {
+    static public int countFives(int[] nums) {
+        Iterator i = new iterable();
+        return 1;
+    }
+}
