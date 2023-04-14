b4fdd8fa7d9c747b5cccec5b02da7406ae0b4521
Renso Guilalas <rensoglorenzo@gmail.com>
2023-04-14 16:57:29 +0100

    Working on project.

--- JavaSoc/ClientApp.java
+++ JavaSoc/ClientApp.java
@@ -1,4 +1,12 @@
+/*
+* Ideas:
+* - The Activity class will extend StreamObject.
+* - Each activity will be a class that extends StreamObject and just pick whichever fields are revelevant to them.
+* - The activiies will have constructors where one of the args are of type Person, where we will be able to access the Person's details and conect them to the activity.
+* - Inbox and Outbox will be lists of activities.
+*/
 import java.util.List;
+import java.util.ArrayList;
 import java.time.LocalDate;
 
 interface Activity {
@@ -28,7 +36,7 @@ interface SendMessage {
     boolean send(Activity activity);
 }
 
-/* removes and delivers the next message from inbox */
+/* removes and delivers the next message from outbox */
 interface DeliverNextMessage {
     // returns an Activity, or null if there are no messages
     Activity deliverNext();
@@ -43,7 +51,9 @@ interface Outbox extends SendMessage, DeliverNextMessage {
 /* the client App that handles inboxes and outboxes */
 interface App {
     Inbox getInbox(); // retrieves the inbox
+
     Outbox getOutbox(); // retrievs the outbox
+
     String demo(); // prints a demo of the app in action
 }
 
@@ -51,14 +61,38 @@ enum Audience {
     GLOBAL, FRIENDS, CLOSEFRIENDS, PERSONAL
 }
 
-class Person {
+class Person implements Outbox {
+    // Inbox implementation
+
+    // Outbox implementation
+    List<Activity> outbox = new ArrayList<Activity>();
+
+    // sends a message and adds it to the Outbox
+    public boolean send(Activity activity) {
+        outbox.add(activity);
+        return true;
+    }
+
+    // returns an Activity, or null if there are no messages
+    public Activity deliverNext() {
+        if (getOutbox().isEmpty()) {
+            return null;
+        }
+        return getOutbox().remove(0);
+    }
+
+    // returns count of unsent messages in outbox
+    public int getCount() {
+        return getOutbox().size();
+    }
+
+    // Person fields
     static int counter = 0;
     String name;
     String username;
     String URI = "https://JavaSoc.com/";
     String bio;
     Inbox inbox;
-    Outbox outbox;
     List<Person> followers;
     List<Person> following;
     List<Person> liked;
@@ -92,6 +126,10 @@ class Person {
         return this.liked;
     }
 
+    public List<Activity> getOutbox() {
+        return this.outbox;
+    }
+
     // setters
     public void setName(String name) {
         this.name = name;
@@ -107,10 +145,6 @@ class Person {
 
     public void setInbox(Inbox inbox) {
         this.inbox = inbox;
-    }
-
-    public void setOutbox(Outbox outbox) {
-        this.outbox = outbox;
     }
 
     public void setFollowers(List<Person> followers) {
@@ -135,47 +169,205 @@ class Person {
         setUsername(username);
     }
 
-    Person(String name, String username, String bio, Inbox inbox, Outbox outbox, List<Person> followers, List<Person> following, List<Person> liked) {
+    Person(String name, String username, String bio, Inbox inbox, Outbox outbox, List<Person> followers,
+            List<Person> following, List<Person> liked) {
         counter += 1;
         setName(name);
         setUsername(username);
         setBio(bio);
         setInbox(inbox);
-        setOutbox(outbox);
         setFollowers(followers);
         setFollowing(following);
         setLiked(liked);
     }
 
     public String toString() {
-        return "Person"+counter+" added"+"\n"+
-               "- URI: "+getURI()+getUsername()+"\n" +
-               "- name: "+getName()+"\n";
+        return "Person" + counter + " added" + "\n" +
+                "- URI: " + getURI() + getUsername() + "\n" +
+                "- name: " + getName() + "\n\n";
     }
 }
 
-class StreamObject implements Activity{
-    String URI;
+class StreamObject implements Activity {
+    String URI = "https://JavSoc.com/";
     String attatchment;
     String attributedTo;
+    String type;
+    String content;
+    String description;
     Audience audience;
     int likes;
     int shares;
-    String content;
     String name;
+    String username;
     LocalDate published;
-    LocalDate deleted;
+    boolean deleted;
 
     public String getURI() {
         return this.URI;
     }
+
+    public String getAttatchment() {
+        return this.attatchment;
+    }
+
+    public String getAttributedTo() {
+        return this.attributedTo;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Audience getAudience() {
+        return this.audience;
+    }
+
+    public int getLikes() {
+        return this.likes;
+    }
+
+    public int getShares() {
+        return this.shares;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public LocalDate getPublished() {
+        return this.published;
+    }
+
+    public boolean getDeleted() {
+        return this.deleted;
+    }
+
+    // Setters
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+
+    public void setAttachment(String attatchment) {
+        this.attatchment = attatchment;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setDesciption(String description) {
+        this.description = description;
+    }
+
+    public void setAudience(Audience audience) {
+        this.audience = audience;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPublished(LocalDate published) {
+        this.published = published;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
 
-class ActivityObject implements Activity {
-    String URI;
+// Implement actual activity, e.g. Like, Post, which extends StreamObject
+class PostActivity extends StreamObject {
+    static int count = 0;
 
-    public String getURI() {
-        return this.URI;
+    // Constructors
+    PostActivity() {
+    }
+
+    PostActivity(Person person, String type, String content, Audience audience, int likes, int shares,
+            LocalDate published) {
+        count += 1;
+        setURI(URI + person.username + "/post" + count);
+        setUsername(person.username);
+        setType(type);
+        setContent(content);
+        setAudience(audience);
+        setLikes(likes);
+        setShares(shares);
+        setPublished(published);
+        setDeleted(false);
+    }
+
+    PostActivity(Person person, boolean deleted) {
+        setURI(URI);
+        setUsername(person.username);
+        setDeleted(true);
+    }
+
+    public String toString() {
+        if (deleted) {
+            return username + " added a " + type + " to Outbox\n" +
+                    "- Post was deleted.\n\n";
+        }
+        return username + " added a " + type + " to Outbox\n" +
+                "- URI: " + getURI() + "\n" +
+                "- audience: " + getAudience() + "\n" +
+                "- content: " + getContent() + "\n" +
+                "- likes: " + getLikes() + "\n" +
+                "- shares: " + getShares() + "\n" +
+                "- published: " + getPublished() + "\n" +
+                "- deleted: " + getDeleted() + "\n" +
+                "\n";
+    }
+}
+
+// Implement Inbox and Outbox
+class Out implements Outbox {
+    List<Activity> outbox = new ArrayList<Activity>();
+
+    public boolean send(Activity activity) {
+        outbox.add(activity);
+        return true;
+    }
+
+    public Activity deliverNext() {
+        if (outbox.isEmpty()) {
+            return null;
+        }
+        return outbox.remove(0);
+    }
+
+    public int getCount() {
+        return outbox.size();
     }
 }
 
@@ -192,22 +384,37 @@ class ClientApp implements App {
     }
 
     public String demo() {
-        return "";
-    }
-}
+        String output = "";
+        LocalDate today = LocalDate.now();
 
-class Test {
-    public static void main(String args[]) {
         Person p = new Person("Renso", "osner");
-        System.out.println(p);
+        output += p;
 
-        Person q = new Person("Troller", "DaTroll");
-        System.out.println(q);
+        // Person q = new Person("Troller", "DaTroll");
+        // output += q;
 
-        Person r = new Person();
-        System.out.println(r);
+        // Adding an activity to the outbox
+        PostActivity post = new PostActivity(p, "Note", "This is the first note in JavaSoc", Audience.GLOBAL, 0, 0, today);
+        p.send(post);
+        output += post;
+        //output += p.getCount() + "\n";
 
-        //ClientApp x = new ClientApp();
-        //System.out.println(x.demo());
+        // Removing Activity from the outbox
+        output += p.deliverNext();
+        output += p.username + " Outbox delivery\n"+
+                "- " + p.getURI() + " posted\n";
+
+        // checking if there are activities in the outbox
+        // output += p.getCount();
+
+        // PostActivity post1 = new PostActivity(q, true);
+        // output += post1;
+
+        return output;
+    }
+
+    public static void main(String args[]) {
+        ClientApp x = new ClientApp();
+        System.out.println(x.demo());
     }
 }
