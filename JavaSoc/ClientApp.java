7e24c867222d7b2874c267ce7b346552f64c8024
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-04-14 12:14:00 +0100

    Working on project.

--- JavaSoc/ClientApp.java
+++ JavaSoc/ClientApp.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.time.LocalDate;
 
 interface Activity {
     String getURI();
@@ -46,6 +47,138 @@ interface App {
     String demo(); // prints a demo of the app in action
 }
 
+enum Audience {
+    GLOBAL, FRIENDS, CLOSEFRIENDS, PERSONAL
+}
+
+class Person {
+    static int counter = 0;
+    String name;
+    String username;
+    String URI = "https://JavaSoc.com/";
+    String bio;
+    Inbox inbox;
+    Outbox outbox;
+    List<Person> followers;
+    List<Person> following;
+    List<Person> liked;
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getURI() {
+        return this.URI;
+    }
+
+    public String bio() {
+        return this.bio;
+    }
+
+    public List<Person> getFollowers() {
+        return this.followers;
+    }
+
+    public List<Person> getFollowing() {
+        return this.following;
+    }
+
+    public List<Person> getLiked() {
+        return this.liked;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setInbox(Inbox inbox) {
+        this.inbox = inbox;
+    }
+
+    public void setOutbox(Outbox outbox) {
+        this.outbox = outbox;
+    }
+
+    public void setFollowers(List<Person> followers) {
+        this.followers = followers;
+    }
+
+    public void setFollowing(List<Person> following) {
+        this.following = following;
+    }
+
+    public void setLiked(List<Person> liked) {
+        this.liked = liked;
+    }
+
+    Person() {
+        counter += 1;
+    }
+
+    Person(String name, String username) {
+        counter += 1;
+        setName(name);
+        setUsername(username);
+    }
+
+    Person(String name, String username, String bio, Inbox inbox, Outbox outbox, List<Person> followers, List<Person> following, List<Person> liked) {
+        counter += 1;
+        setName(name);
+        setUsername(username);
+        setBio(bio);
+        setInbox(inbox);
+        setOutbox(outbox);
+        setFollowers(followers);
+        setFollowing(following);
+        setLiked(liked);
+    }
+
+    public String toString() {
+        return "Person"+counter+" added"+"\n"+
+               "- URI: "+getURI()+getUsername()+"\n" +
+               "- name: "+getName()+"\n";
+    }
+}
+
+class StreamObject implements Activity{
+    String URI;
+    String attatchment;
+    String attributedTo;
+    Audience audience;
+    int likes;
+    int shares;
+    String content;
+    String name;
+    LocalDate published;
+    LocalDate deleted;
+
+    public String getURI() {
+        return this.URI;
+    }
+}
+
+class ActivityObject implements Activity {
+    String URI;
+
+    public String getURI() {
+        return this.URI;
+    }
+}
+
 class ClientApp implements App {
     public Inbox inbox;
     public Outbox outbox;
@@ -59,67 +192,7 @@ class ClientApp implements App {
     }
 
     public String demo() {
-        return "This should include your demo output.";
-    }
-}
-
-class Person {
-    String name;
-    String username;
-    String bio;
-    Inbox inbox;
-    Outbox outbox;
-    List followers;
-    List following;
-    List liked;
-
-    // getters
-    public String getName() {
-        return this.name;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String bio() {
-        return this.bio;
-    }
-
-    public List getFollowers() {
-        return this.followers;
-    }
-
-    public List getFollowing() {
-        return this.following;
-    }
-
-    public List getLiked() {
-        return this.liked;
-    }
-
-    // setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    Person() {
-        setName("");
-        setUsername("");
-    }
-
-    Person(String name, String username) {
-        setName(name);
-        setUsername(username);
-    }
-
-    public String toString() {
-        return "Name: "+getName()+"\n" +
-               "Username: "+getUsername()+"\n";
+        return "";
     }
 }
 
@@ -128,7 +201,13 @@ class Test {
         Person p = new Person("Renso", "osner");
         System.out.println(p);
 
-        Person q = new Person();
+        Person q = new Person("Troller", "DaTroll");
         System.out.println(q);
+
+        Person r = new Person();
+        System.out.println(r);
+
+        //ClientApp x = new ClientApp();
+        //System.out.println(x.demo());
     }
 }
